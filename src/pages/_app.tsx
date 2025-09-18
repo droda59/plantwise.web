057382b401c6733d0b4d3af511d41c00edf3ff8a
droda59 <swcommander@gmail.com>
2025-09-18 16:57:30 -0400

    Fixed the fucking layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-// Should not need this
 import '@/styles/globals.css';
 import RootLayout from './root-layout';
 
 export default function App({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => (
-        <RootLayout>{page}</RootLayout>
+        <RootLayout>
+            {page}
+        </RootLayout>
     ));
     return getLayout(<Component {...pageProps} />);
 }
