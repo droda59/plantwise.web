03e1312e27b4b0a32c7340647729f6097262cf58
droda59 <swcommander@gmail.com>
2025-10-06 22:34:44 -0400

    Disabled select label when disabled

--- src/components/filter-item.tsx
+++ src/components/filter-item.tsx
@@ -21,7 +21,11 @@ export const SidebarMenuFilterItem = ({
     ...props
 }: SidebarMenuFilterItemProps) => (
     <SidebarMenuItem>
-        <div className={`flex items-center gap-2 overflow-hidden rounded-md py-2 px-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground group-has-data-[sidebar=menu-action]/menu-item:pr-8 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 ${props.disabled && 'text-muted'}`}>
+        <div className={`flex items-center gap-2 overflow-hidden rounded-md py-2 px-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] 
+            ${props.disabled
+                ? 'text-muted hover:text-muted data-[state=open]:hover:text-muted'
+                : 'data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground'}
+            focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground group-has-data-[sidebar=menu-action]/menu-item:pr-8 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0`}>
             <Icon />
             {title && <span>{title}</span>}
             {label && <label className='grow' htmlFor={target}>{label}</label>}
