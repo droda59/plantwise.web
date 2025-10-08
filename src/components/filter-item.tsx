a9b079df913d919b3f8fa44a9a55b0c5a80f8a4f
droda59 <swcommander@gmail.com>
2025-10-08 10:04:09 -0400

    Fixed error

--- src/components/filter-item.tsx
+++ src/components/filter-item.tsx
@@ -26,7 +26,7 @@ export const SidebarMenuFilterItem = ({
                 ? 'text-muted hover:text-muted data-[state=open]:hover:text-muted'
                 : 'data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground'}
             focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground group-has-data-[sidebar=menu-action]/menu-item:pr-8 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0`}>
-            <Icon />
+            {Icon && <Icon />}
             {title && <span>{title}</span>}
             {label && <label className='grow' htmlFor={target}>{label}</label>}
             {props.children}
